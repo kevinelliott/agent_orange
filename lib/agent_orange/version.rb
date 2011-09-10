5b904289bcb80d70c026d33d455e166fd7179324
Kevin Elliott <kevin@welikeinc.com>
2011-09-10 14:48:51 -0700

    Fixed so that rake install works now.

--- lib/agent_orange/version.rb
+++ lib/agent_orange/version.rb
@@ -1,9 +1,11 @@
-class AgentOrange::Version
+module AgentOrange
   VERSION = "0.0.1" # This is for the gem and does not conflict with the rest of the functionality
-
-  attr_accessor :major, :minor, :patch_level, :build_number
   
-  def to_s
-    "#{self.major}.#{self.minor}"
+  class Version
+    attr_accessor :major, :minor, :patch_level, :build_number
+  
+    def to_s
+      "#{self.major}.#{self.minor}"
+    end
   end
 end
