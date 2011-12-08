7f47f3daf462c01030fef5fc2e5526b1184c3acd
Kevin Elliott <kevin@welikeinc.com>
2011-12-08 13:09:24 -0800

    Updated README to include brain-geek's contribution, call for action for project rename, and bumping version.

--- lib/agent_orange/version.rb
+++ lib/agent_orange/version.rb
@@ -1,5 +1,5 @@
 module AgentOrange
-  VERSION = "0.1.2" # This is for the gem and does not conflict with the rest of the functionality
+  VERSION = "0.1.3" # This is for the gem and does not conflict with the rest of the functionality
 
   class Version
     attr_accessor :major, :minor, :patch_level, :build_number
